ad1e8fbb6291c7e78b6d28e349e76b0531e059e8
Singh <harjeevs17@gmail.com>
2022-08-23 23:46:10 +0530

    made changes

--- routes/user.js
+++ routes/user.js
@@ -9,100 +9,100 @@ const User = mongoose.model("User");
 
 //Create a new user and insert in DB
 router.post("/signup", (req, res) => {
-    const { name, email, password, phone } = req.body;
-    if (!name || !email || !password || !phone) {
-      return res.json({ error: "Please enter all fields" });
-    }
-    User.findOne({ email: email })
-      .then((savedUser) => {
-        if (savedUser) {
-          return res.json({ error: "User exists with this email" });
-        }
-        bcrypt.hash(password, 12).then((hashedPassword) => {
-          const user = new User({
-            name: name,
-            email: email,
-            password: hashedPassword,
-            phone: phone,
-          });
-          user
-            .save()
-            .then((user) => {
-              return res.json({ message: "User Inserted in DB" });
-            })
-            .catch((err) => {
-              console.log(err);
-            });
+  const { name, email, password, phone } = req.body;
+  if (!name || !email || !password || !phone) {
+    return res.json({ error: "Please enter all fields" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.json({ error: "User exists with this email" });
+      }
+      bcrypt.hash(password, 12).then((hashedPassword) => {
+        const user = new User({
+          name: name,
+          email: email,
+          password: hashedPassword,
+          phone: phone,
         });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    //res.json({ message: "successfully posted" });
-  });
-
-//Retrieve user from the DB based upon email and password
-  router.post("/signin", (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.json({ message: "Enter the given fields" });
-    }
-    User.findOne({ email: email })
-      .then((savedUser) => {
-        if (!savedUser) {
-          return res.json({ error: "Given email is not registered" });
-        }
-        bcrypt
-          .compare(password, savedUser.password)
-          .then((matched) => {
-            if (!matched) {
-              return res.json({ error: "Password did not match" });
-            } else {
-              /*res.json({ message: "User logged in" });
-              return savedUser;*/
-              //console.log(savedUser);
-              const {
-                _id,
-                name,
-                email,
-                phone,
-                address,
-                cart
-              } = savedUser;
-              const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
-              return res.json({
-                token: token,
-                user: { _id, name, email, phone, address,cart },
-              });
-            }
+        user
+          .save()
+          .then((user) => {
+            return res.json({ message: "User Inserted in DB" });
           })
           .catch((err) => {
             console.log(err);
           });
-      })
-      .catch((err) => {
-        console.log(err);
       });
-  });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //res.json({ message: "successfully posted" });
+});
 
-
- //Add address to existing users
- router.post("/addAddress", (req, res) => {
-  const { contactName,contactPhone,addressline1,addressline2,landmark } = req.body.address;
-  if (!contactName || !contactPhone || !addressline1 || !addressline2) {
-    return res.json({ message: "Enter all the fields" });
+//Retrieve user from the DB based upon email and password
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.json({ message: "Enter the given fields" });
   }
-  const data  = {
-      "contactName":contactName,
-      "contactPhone":contactPhone,
-      "addressline1":addressline1,
-      "addressline2":addressline2,
-      "landmark":landmark
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (!savedUser) {
+        return res.json({ error: "Given email is not registered" });
+      }
+      bcrypt
+        .compare(password, savedUser.password)
+        .then((matched) => {
+          if (!matched) {
+            return res.json({ error: "Password did not match" });
+          } else {
+            /*res.json({ message: "User logged in" });
+            return savedUser;*/
+            //console.log(savedUser);
+            const {
+              _id,
+              name,
+              email,
+              phone,
+              address,
+              cart
+            } = savedUser;
+            const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+            return res.json({
+              token: token,
+              user: { _id, name, email, phone, address, cart },
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+//Add address to existing users
+router.post("/addAddress", (req, res) => {
+  const { contactName, contactPhone, addressline1, addressline2, landmark } = req.body.address;
+  if (!contactName || !contactPhone || !addressline1 || !addressline2) {
+    return res.json({ message: "Enter all the fields." });
+  }
+  const data = {
+    "contactName": contactName,
+    "contactPhone": contactPhone,
+    "addressline1": addressline1,
+    "addressline2": addressline2,
+    "landmark": landmark
   }
   User.findByIdAndUpdate(
     req.body._id,
     {
-      $push: { address: data } ,
+      $push: { address: data },
     },
     (err, result) => {
       if (err) {
@@ -110,46 +110,47 @@ router.post("/signup", (req, res) => {
       }
       return res.json(result);
     }
-  )});
-       
-  //Add product inside cart
-  router.post("/addCart", (req, res) => {
-    const { _id,product } = req.body;
-    if (!product) {
-      return res.json({ message: "Product id cannot be empty" });
-    }
-    User.findByIdAndUpdate(
-      _id,
-      {
-        $push: { cart: mongoose.Types.ObjectId(product)} ,
-      },
-      (err, result) => {
-        if (err) {
-          return res.json({ error: err });
-        }
-        return res.json(result);
-      }
-    )  
-  });
+  )
+});
 
-  //Delete product from cart
-  router.post("/deleteCart", (req, res) => {
-    const { _id,product } = req.body;
-    if (!product) {
-      return res.json({ message: "Product id cannot be empty" });
-    }
-    User.findByIdAndUpdate(
-      _id,
-      {
-        $pull: { cart: mongoose.Types.ObjectId(product)} ,
-      },
-      (err, result) => {
-        if (err) {
-          return res.json({ error: err });
-        }
-        return res.json(result);
+//Add product inside cart
+router.post("/addCart", (req, res) => {
+  const { _id, product } = req.body;
+  if (!product) {
+    return res.json({ message: "Product id cannot be empty" });
+  }
+  User.findByIdAndUpdate(
+    _id,
+    {
+      $push: { cart: mongoose.Types.ObjectId(product) },
+    },
+    (err, result) => {
+      if (err) {
+        return res.json({ error: err });
       }
-    )  
-  });
-  
-  module.exports = router;
+      return res.json(result);
+    }
+  )
+});
+
+//Delete product from cart
+router.post("/deleteCart", (req, res) => {
+  const { _id, product } = req.body;
+  if (!product) {
+    return res.json({ message: "Product id cannot be empty" });
+  }
+  User.findByIdAndUpdate(
+    _id,
+    {
+      $pull: { cart: mongoose.Types.ObjectId(product) },
+    },
+    (err, result) => {
+      if (err) {
+        return res.json({ error: err });
+      }
+      return res.json(result);
+    }
+  )
+});
+
+module.exports = router;
